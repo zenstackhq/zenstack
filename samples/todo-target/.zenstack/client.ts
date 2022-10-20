5095960e1fefa388ec0875ec685ed525e0728851
Yiming Cao <yiming@whimslab.io>
2022-10-20 17:48:24 +0800

    sync versions

--- samples/todo-target/.zenstack/client.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from './.prisma';
-
-export class ZenStackClient {
-    readonly prisma = new PrismaClient();
-
-    constructor() {}
-}
-
-export default new ZenStackClient();
