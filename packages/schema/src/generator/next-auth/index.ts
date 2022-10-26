48169b09437451a18da5d58fca361ea8578b383e
Yiming Cao <yiming@whimslab.io>
2022-10-26 16:04:27 +0800

    feat: adaptive code generation for next-auth

--- packages/schema/src/generator/next-auth/index.ts
+++ packages/schema/src/generator/next-auth/index.ts
@@ -2,12 +2,56 @@ import { Context, Generator } from '../types';
 import { Project } from 'ts-morph';
 import * as path from 'path';
 import colors from 'colors';
+import { DataModel, isDataModel, Model } from '@lang/generated/ast';
 
 /**
  * Generates NextAuth adaptor code
  */
 export default class NextAuthGenerator implements Generator {
+    private findModel(schema: Model, name: string) {
+        return schema.declarations.find(
+            (d) => isDataModel(d) && d.name === name
+        ) as DataModel;
+    }
+
+    private modelHasField(model: DataModel, name: string) {
+        return !!model.fields.find((f) => f.name === name);
+    }
+
     async generate(context: Context): Promise<void> {
+        try {
+            require('next-auth');
+        } catch (err) {
+            console.warn(
+                colors.yellow(
+                    'Next-auth module is not installed, skipping generating adapter.'
+                )
+            );
+            return;
+        }
+
+        if (!this.findModel(context.schema, 'User')) {
+            console.warn(
+                colors.yellow(
+                    'Skipping generating next-auth adapter: "User" model not found.'
+                )
+            );
+            return;
+        }
+
+        const userModel = this.findModel(context.schema, 'User');
+        if (
+            !this.modelHasField(userModel, 'email') ||
+            !this.modelHasField(userModel, 'emailVerified')
+        ) {
+            console.warn(
+                colors.yellow(
+                    `Skipping generating next-auth adapter because "User" model doesn't meet requirements: "email" and "emailVerified" fields are required.`
+                )
+            );
+            return;
+        }
+
         const project = new Project();
 
         this.generateIndex(project, context);
@@ -35,124 +79,213 @@ export default class NextAuthGenerator implements Generator {
     }
 
     private generateAdapter(project: Project, context: Context) {
-        const content = `
-        import { ZenStackService } from '..';
-        import { Adapter } from 'next-auth/adapters';
-        import { Prisma } from '../../.prisma';
-        
-        export function NextAuthAdapter(service: ZenStackService): Adapter {
-            const db = service.db;
-            return {
-                createUser: (data) => db.user.create({ data: data as Prisma.UserCreateInput }),
-                getUser: (id) => db.user.findUnique({ where: { id } }),
-                getUserByEmail: (email) => db.user.findUnique({ where: { email } }),
-                async getUserByAccount(provider_providerAccountId) {
-                    const account = await db.account.findUnique({
-                        where: { provider_providerAccountId },
-                        select: { user: true },
-                    });
-                    return account?.user ?? null;
-                },
-                updateUser: (data) => db.user.update({ where: { id: data.id }, data: data as Prisma.UserUpdateInput }),
-                deleteUser: (id) => db.user.delete({ where: { id } }),
-                linkAccount: (data) => db.account.create({ data }) as any,
-                unlinkAccount: (provider_providerAccountId) =>
-                    db.account.delete({ where: { provider_providerAccountId } }) as any,
-                async getSessionAndUser(sessionToken) {
-                    const userAndSession = await db.session.findUnique({
-                        where: { sessionToken },
-                        include: { user: true },
-                    });
-                    if (!userAndSession) return null;
-                    const { user, ...session } = userAndSession;
-                    return { user, session };
-                },
-                createSession: (data) => db.session.create({ data }),
-                updateSession: (data) =>
-                    db.session.update({
-                        data,
-                        where: { sessionToken: data.sessionToken },
-                    }),
-                deleteSession: (sessionToken) =>
-                    db.session.delete({ where: { sessionToken } }),
-                createVerificationToken: (data) => db.verificationToken.create({ data }),
-                async useVerificationToken(identifier_token) {
-                    try {
-                        return await db.verificationToken.delete({
-                            where: { identifier_token },
-                        });
-                    } catch (error) {
-                        // If token already used/deleted, just return null
-                        // https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
-                        if (
-                            (error as Prisma.PrismaClientKnownRequestError).code ===
-                            'P2025'
-                        )
-                            return null;
-                        throw error;
-                    }
-                },
-            };
-        }                
-        `;
-
         const sf = project.createSourceFile(
             path.join(
                 context.generatedCodeDir,
                 'src/auth/next-auth-adapter.ts'
             ),
-            content,
+            undefined,
             { overwrite: true }
         );
+
+        sf.addImportDeclarations([
+            {
+                namedImports: [
+                    {
+                        name: 'ZenStackService',
+                    },
+                ],
+                moduleSpecifier: '..',
+            },
+            {
+                namedImports: [
+                    {
+                        name: 'Adapter',
+                    },
+                ],
+                moduleSpecifier: 'next-auth/adapters',
+            },
+            {
+                namedImports: [
+                    {
+                        name: 'Prisma',
+                    },
+                ],
+                moduleSpecifier: '../../.prisma',
+                isTypeOnly: true,
+            },
+        ]);
+
+        const adapter = sf.addFunction({
+            name: 'NextAuthAdapter',
+            isExported: true,
+            returnType: 'Adapter',
+        });
+
+        adapter.addParameter({
+            name: 'service',
+            type: 'ZenStackService',
+        });
+
+        const userModel = this.findModel(context.schema, 'User');
+
+        adapter.setBodyText((writer) => {
+            writer.writeLine('const db = service.db;');
+            writer.write('return ');
+            writer.block(() => {
+                writer.writeLine(
+                    `
+                    createUser: (data) => db.user.create({ data: data as Prisma.UserCreateInput }),
+                    getUser: (id) => db.user.findUnique({ where: { id } }),
+                    updateUser: (data) => db.user.update({ where: { id: data.id }, data: data as Prisma.UserUpdateInput }),
+                    deleteUser: (id) => db.user.delete({ where: { id } }),
+                    `
+                );
+
+                if (this.modelHasField(userModel, 'email')) {
+                    writer.writeLine(
+                        'getUserByEmail: (id) => db.user.findUnique({ where: { id } }),'
+                    );
+                } else {
+                    writer.writeLine(
+                        `getUserByEmail: (id) => { throw new Error('"User" model has no "email" field'); },`
+                    );
+                }
+
+                if (this.findModel(context.schema, 'Account')) {
+                    writer.writeLine(
+                        `
+                        async getUserByAccount(provider_providerAccountId) {
+                            const account = await db.account.findUnique({
+                                where: { provider_providerAccountId },
+                                select: { user: true },
+                            });
+                            return account?.user ?? null;
+                        },
+                        linkAccount: (data) => db.account.create({ data }) as any,
+                        unlinkAccount: (provider_providerAccountId) =>
+                            db.account.delete({ where: { provider_providerAccountId } }) as any,
+                        `
+                    );
+                } else {
+                    writer.writeLine(
+                        `
+                        async getUserByAccount(provider_providerAccountId) { throw new Error('Schema has no "Account" model declared'); },
+                        linkAccount: (data) => { throw new Error('Schema has no "Account" model declared'); },
+                        unlinkAccount: (provider_providerAccountId) => { throw new Error('Schema has no "Account" model declared'); },
+                        `
+                    );
+                }
+
+                if (this.findModel(context.schema, 'Session')) {
+                    writer.writeLine(
+                        `
+                        async getSessionAndUser(sessionToken) {
+                            const userAndSession = await db.session.findUnique({
+                                where: { sessionToken },
+                                include: { user: true },
+                            });
+                            if (!userAndSession) return null;
+                            const { user, ...session } = userAndSession;
+                            return { user, session };
+                        },
+                        createSession: (data) => db.session.create({ data }),
+                        updateSession: (data) =>
+                            db.session.update({
+                                data,
+                                where: { sessionToken: data.sessionToken },
+                            }),
+                        deleteSession: (sessionToken) =>
+                            db.session.delete({ where: { sessionToken } }),
+                        `
+                    );
+                } else {
+                    writer.writeLine(
+                        `
+                        async getSessionAndUser(sessionToken) { throw new Error('Schema has no "Session" model declared'); },
+                        createSession: (data) => { throw new Error('Schema has no "Session" model declared'); },
+                        updateSession: (data) => { throw new Error('Schema has no "Session" model declared'); },
+                        deleteSession: (sessionToken) => { throw new Error('Schema has no "Session" model declared'); },                        `
+                    );
+                }
+
+                if (this.findModel(context.schema, 'VerificationToken')) {
+                    writer.writeLine(
+                        `
+                        createVerificationToken: (data) => db.verificationToken.create({ data }),
+                        async useVerificationToken(identifier_token) {
+                            try {
+                                return await db.verificationToken.delete({
+                                    where: { identifier_token },
+                                });
+                            } catch (error) {
+                                // If token already used/deleted, just return null
+                                // https://www.prisma.io/docs/reference/api-reference/error-reference#p2025
+                                if (
+                                    (error as Prisma.PrismaClientKnownRequestError).code ===
+                                    'P2025'
+                                )
+                                    return null;
+                                throw error;
+                            }
+                        },
+                        `
+                    );
+                } else {
+                    writer.writeLine(
+                        `
+                        createVerificationToken: (data) => { throw new Error('Schema has no "VerificationToken" model declared'); },
+                        async useVerificationToken(identifier_token) { throw new Error('Schema has no "VerificationToken" model declared'); },                       `
+                    );
+                }
+            });
+        });
 
         sf.formatText();
     }
 
     private generateAuthorize(project: Project, context: Context) {
-        const content = `
-        import { ZenStackService } from '..';
-        import { hash, compare } from 'bcryptjs';
-        
-        async function hashPassword(password: string) {
-            const hashedPassword = await hash(password, 12);
-            return hashedPassword;
-        }
-        
-        async function verifyPassword(password: string, hashedPassword: string) {
-            const isValid = await compare(password, hashedPassword);
-            return isValid;
-        }
-        
-        export function authorize(service: ZenStackService) {
-            return async (
-                credentials: Record<'email' | 'password', string> | undefined
-            ) => {
-                if (!credentials) {
-                    throw new Error('Missing credentials');
-                }
+        const userModel = this.findModel(context.schema, 'User');
+        const hasEmail = userModel && this.modelHasField(userModel, 'email');
+        const hasPassword =
+            userModel && this.modelHasField(userModel, 'password');
 
-                try {
-                    let maybeUser = await service.db.user.findFirst({
-                        where: {
-                            email: credentials!.email,
-                        },
-                        select: {
-                            id: true,
-                            email: true,
-                            password: true,
-                            name: true,
-                        },
-                    });
-        
-                    if (!maybeUser) {
-                        if (!credentials.password || !credentials.email) {
-                            throw new Error('Invalid Credentials');
-                        }
-        
-                        maybeUser = await service.db.user.create({
-                            data: {
-                                email: credentials.email,
-                                password: await hashPassword(credentials.password),
+        let content = '';
+        if (!hasEmail || !hasPassword) {
+            content = `
+            import { ZenStackService } from '..';
+
+            export function authorize(service: ZenStackService, implicitSignup = false) {
+                throw new Error('"User" model must have "email" and "password" field');
+            }
+            `;
+        } else {
+            content = `
+            import { ZenStackService } from '..';
+            import { hash, compare } from 'bcryptjs';
+            
+            async function hashPassword(password: string) {
+                const hashedPassword = await hash(password, 12);
+                return hashedPassword;
+            }
+            
+            async function verifyPassword(password: string, hashedPassword: string) {
+                const isValid = await compare(password, hashedPassword);
+                return isValid;
+            }
+            
+            export function authorize(service: ZenStackService, implicitSignup = false) {
+                return async (
+                    credentials: Record<'email' | 'password', string> | undefined
+                ) => {
+                    if (!credentials) {
+                        throw new Error('Missing credentials');
+                    }
+
+                    try {
+                        let maybeUser = await service.db.user.findFirst({
+                            where: {
+                                email: credentials!.email,
                             },
                             select: {
                                 id: true,
@@ -161,33 +294,52 @@ export default class NextAuthGenerator implements Generator {
                                 name: true,
                             },
                         });
-                    } else {
-                        if (!maybeUser.password) {
-                            throw new Error('Invalid User Record');
-                        }
+            
+                        if (!maybeUser) {
+                            if (!implicitSignup || !credentials.password || !credentials.email) {
+                                return null;
+                            }
+            
+                            maybeUser = await service.db.user.create({
+                                data: {
+                                    email: credentials.email,
+                                    password: await hashPassword(credentials.password),
+                                },
+                                select: {
+                                    id: true,
+                                    email: true,
+                                    password: true,
+                                    name: true,
+                                },
+                            });
+                        } else {
+                            if (!maybeUser.password) {
+                                throw new Error('Invalid User Record');
+                            }
 
-                        const isValid = await verifyPassword(
-                            credentials.password,
-                            maybeUser.password
-                        );
-        
-                        if (!isValid) {
-                            throw new Error('Invalid Credentials');
+                            const isValid = await verifyPassword(
+                                credentials.password,
+                                maybeUser.password
+                            );
+            
+                            if (!isValid) {
+                                return null;
+                            }
                         }
+            
+                        return {
+                            id: maybeUser.id,
+                            email: maybeUser.email,
+                            name: maybeUser.name,
+                        };
+                    } catch (error) {
+                        console.log('Error occurred during authorization:', error);
+                        throw error;
                     }
-        
-                    return {
-                        id: maybeUser.id,
-                        email: maybeUser.email,
-                        name: maybeUser.name,
-                    };
-                } catch (error) {
-                    console.log(error);
-                    throw error;
-                }
-            };
-        }
+                };
+            }
         `;
+        }
 
         const sf = project.createSourceFile(
             path.join(context.generatedCodeDir, 'src/auth/authorize.ts'),
