f97d4c114c0779ef63b8aa3462ddf45e91cebed4
ymc9 <104139426+ymc9@users.noreply.github.com>
2022-11-03 22:51:38 +0800

    feat: add @omit field attribute (#42)
    - add @omit field to indicate a field should be excluded when read
    - update todo sample and remove unnecessary Account/Session/VerificationToken models

--- packages/internal/src/constants.ts
+++ packages/internal/src/constants.ts
@@ -4,6 +4,11 @@
 export const TRANSACTION_FIELD_NAME = 'zenstack_transaction';
 
 /**
+ * Auxiliary database field for building up policy check queries
+ */
+export const GUARD_FIELD_NAME = 'zenstack_guard';
+
+/**
  * Default length of password hash salt (used by bcryptjs to hash password)
  */
 export const DEFAULT_PASSWORD_SALT_LENGTH = 12;
