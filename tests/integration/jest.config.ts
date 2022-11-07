d895e82b897ca859f83555e7d45f8fda29381179
ymc9 <104139426+ymc9@users.noreply.github.com>
2022-11-07 14:00:01 +0800

    changes

--- tests/integration/jest.config.ts
+++ tests/integration/jest.config.ts
@@ -12,7 +12,7 @@ export default {
     // A map from regular expressions to paths to transformers
     transform: { '^.+\\.tsx?$': 'ts-jest' },
 
-    testTimeout: 120000,
+    testTimeout: 300000,
 
     // explicitly specify moduel paths so that resolution from local dependencies work
     modulePaths: ['<rootDir>/tests/test-run/node_modules'],
