d2736c6505cc9e9b259fdb8ecab01a28656aefa9
ymc9 <104139426+ymc9@users.noreply.github.com>
2022-11-07 09:50:22 +0800

    fix: internal error 500 is returned when unique constraint violation happens

--- packages/internal/src/types.ts
+++ packages/internal/src/types.ts
@@ -134,3 +134,31 @@ export enum ServerErrorCode {
      */
     UNKNOWN = 'UNKNOWN',
 }
+
+export function getServerErrorMessage(code: ServerErrorCode): string {
+    switch (code) {
+        case ServerErrorCode.ENTITY_NOT_FOUND:
+            return 'the requested entity is not found';
+
+        case ServerErrorCode.INVALID_REQUEST_PARAMS:
+            return 'request parameters are invalid';
+
+        case ServerErrorCode.DENIED_BY_POLICY:
+            return 'the request was denied due to access policy violation';
+
+        case ServerErrorCode.UNIQUE_CONSTRAINT_VIOLATION:
+            return 'the request failed because of database unique constraint violation';
+
+        case ServerErrorCode.REFERENCE_CONSTRAINT_VIOLATION:
+            return 'the request failed because of database foreign key constraint violation';
+
+        case ServerErrorCode.READ_BACK_AFTER_WRITE_DENIED:
+            return 'the write operation succeeded, but the data cannot be read back due to access policy violation';
+
+        case ServerErrorCode.UNKNOWN:
+            return 'an unknown error occurred';
+
+        default:
+            return `generic error: ${code}`;
+    }
+}
