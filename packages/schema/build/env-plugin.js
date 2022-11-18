bb9b98f3a6729c3936e6b5626bd19b9ae556829e
ymc9 <104139426+ymc9@users.noreply.github.com>
2022-11-18 18:26:36 +0800

    fix: fix esbuild plugin

--- packages/schema/build/env-plugin.js
+++ packages/schema/build/env-plugin.js
@@ -10,26 +10,46 @@ module.exports = {
 
     setup(build) {
         function _findEnvFile(dir) {
-            if (!fs.existsSync(dir)) return false;
-            return fs.existsSync(`${dir}/.env.${ENV}`)
-                ? `${dir}/.env.${ENV}`
-                : fs.existsSync(`${dir}/.env`)
-                ? `${dir}/.env`
-                : _findEnvFile(path.resolve(dir, '../'));
+            if (!fs.existsSync(dir)) return undefined;
+
+            if (fs.existsSync(`${dir}/.env.${ENV}`)) {
+                return `${dir}/.env.${ENV}`;
+            } else if (fs.existsSync(`${dir}/.env`)) {
+                return `${dir}/.env`;
+            } else {
+                const next = path.resolve(dir, '../');
+                if (next === dir) {
+                    // at root now, exit
+                    return undefined;
+                } else {
+                    return _findEnvFile(next);
+                }
+            }
         }
 
         build.onResolve({ filter: /^env$/ }, async (args) => {
+            const envPath = _findEnvFile(args.resolveDir);
             return {
-                path: _findEnvFile(args.resolveDir),
+                path: args.path,
                 namespace: 'env-ns',
+                pluginData: {
+                    ...args.pluginData,
+                    envPath,
+                },
             };
         });
 
         build.onLoad({ filter: /.*/, namespace: 'env-ns' }, async (args) => {
             // read in .env file contents and combine with regular .env:
-            let data = await fs.promises.readFile(args.path, 'utf8');
-            const buf = Buffer.from(data);
-            const config = require('dotenv').parse(buf);
+            let config = {};
+            if (args.pluginData && args.pluginData.envPath) {
+                let data = await fs.promises.readFile(
+                    args.pluginData.envPath,
+                    'utf8'
+                );
+                const buf = Buffer.from(data);
+                config = require('dotenv').parse(buf);
+            }
 
             return {
                 contents: JSON.stringify({ ...process.env, ...config }),
