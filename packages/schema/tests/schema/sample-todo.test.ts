00ce7798e33d50a1cc267782899da0429396a9be
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-01-23 10:09:11 +0800

    fix test

--- packages/schema/tests/schema/sample-todo.test.ts
+++ packages/schema/tests/schema/sample-todo.test.ts
@@ -1,9 +1,10 @@
 import * as fs from 'fs';
+import path from 'path';
 import { loadModel } from '../utils';
 
 describe('Basic Tests', () => {
     it('sample todo schema', async () => {
-        const content = fs.readFileSync('../../samples/todo/schema.zmodel', {
+        const content = fs.readFileSync(path.join(__dirname, './todo.zmodel'), {
             encoding: 'utf-8',
         });
         await loadModel(content);
