ad6bc2c4ca28479be4b360c6b6cbf2c65c5d0de6
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-01-27 11:07:52 +0800

    fix: fix telemetry token replacement at build time

--- packages/schema/build/post-build.js
+++ packages/schema/build/post-build.js
@@ -2,14 +2,16 @@ require('dotenv').config({ path: './.env.local' });
 require('dotenv').config({ path: './.env' });
 const fs = require('fs');
 
-if (process.env.TELEMETRY_TRACKING_TOKEN) {
-    let postInstallContent = fs.readFileSync('bin/post-install.js', 'utf-8');
-    postInstallContent = postInstallContent.replace('<TELEMETRY_TRACKING_TOKEN>', process.env.TELEMETRY_TRACKING_TOKEN);
-    fs.writeFileSync('dist/bin/post-install.js', postInstallContent, {
-        encoding: 'utf-8',
-    });
-} else {
-    fs.writeFileSync('dist/bin/post-install.js', '', {
+const filesToReplace = ['dist/bin/post-install.js', 'dist/constants.js'];
+for (const file of filesToReplace) {
+    let content = fs.readFileSync(file, 'utf-8');
+    if (process.env.TELEMETRY_TRACKING_TOKEN) {
+        content = content.replace('<TELEMETRY_TRACKING_TOKEN>', process.env.TELEMETRY_TRACKING_TOKEN);
+    } else {
+        content = content.replace('<TELEMETRY_TRACKING_TOKEN>', '');
+    }
+    console.log('Updating file:', file);
+    fs.writeFileSync(file, content, {
         encoding: 'utf-8',
     });
 }
@@ -20,6 +22,8 @@ if (process.env.DEFAULT_NPM_TAG) {
 } else {
     cliContent = cliContent.replace('<DEFAULT_NPM_TAG>', 'latest');
 }
+
+console.log('Updating file: dist/cli/index.js');
 fs.writeFileSync('dist/cli/index.js', cliContent, {
     encoding: 'utf-8',
 });
