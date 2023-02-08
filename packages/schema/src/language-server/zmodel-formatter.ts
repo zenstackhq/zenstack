6230dc1ecf1b7b7b86cb79bc02cf16cda1077f95
Jiasheng <jiashengguo@outlook.com>
2023-02-08 09:51:56 +0000

    feat: Support code action of generating opposite relation fields (#201)

--- packages/schema/src/language-server/zmodel-formatter.ts
+++ packages/schema/src/language-server/zmodel-formatter.ts
@@ -1,13 +1,16 @@
-import { AbstractFormatter, AstNode, Formatting } from 'langium';
+import { AbstractFormatter, AstNode, Formatting, LangiumDocument } from 'langium';
 
 import * as ast from '@zenstackhq/language/ast';
+import { FormattingOptions, Range, TextEdit } from 'vscode-languageserver';
 
 export class ZModelFormatter extends AbstractFormatter {
+    private formatOptions?: FormattingOptions;
     protected format(node: AstNode): void {
         const formatter = this.getNodeFormatter(node);
         if (ast.isAbstractDeclaration(node)) {
             const bracesOpen = formatter.keyword('{');
             const bracesClose = formatter.keyword('}');
+            // this line decide the indent count return by this.getIndent()
             formatter.interior(bracesOpen, bracesClose).prepend(Formatting.indent());
             bracesOpen.prepend(Formatting.oneSpace());
             bracesClose.prepend(Formatting.newLine());
@@ -16,5 +19,22 @@ export class ZModelFormatter extends AbstractFormatter {
             const nodes = formatter.nodes(...model.declarations);
             nodes.prepend(Formatting.noIndent());
         }
+    }
+
+    protected override doDocumentFormat(
+        document: LangiumDocument<AstNode>,
+        options: FormattingOptions,
+        range?: Range | undefined
+    ): TextEdit[] {
+        this.formatOptions = options;
+        return super.doDocumentFormat(document, options, range);
+    }
+
+    public getFormatOptions(): FormattingOptions | undefined {
+        return this.formatOptions;
+    }
+
+    public getIndent() {
+        return 1;
     }
 }
