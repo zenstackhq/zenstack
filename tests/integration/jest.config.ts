3c9b0f75651dc12e0d31ba4432d66cf8c9641b4d
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-03-03 14:39:23 +0800

    chore: enable codecov

--- tests/integration/jest.config.ts
+++ tests/integration/jest.config.ts
@@ -14,4 +14,19 @@ export default {
     globalSetup: './global-setup.js',
 
     setupFilesAfterEnv: ['./test-setup.ts'],
+
+    // Indicates whether the coverage information should be collected while executing the test
+    collectCoverage: true,
+
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'tests/coverage',
+
+    // An array of regexp pattern strings used to skip coverage collection
+    coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+
+    // Indicates which provider should be used to instrument code for coverage
+    coverageProvider: 'v8',
+
+    // A list of reporter names that Jest uses when writing coverage reports
+    coverageReporters: ['json', 'text', 'lcov', 'clover'],
 };
