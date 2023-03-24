4137712e089d0cf6974c210f3de3183fdc1bc537
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-03-24 10:14:47 +0800

    add tests and try out buildjet CI runner

---
+++ tests/integration/tests/e2e/filter-function-coverage.test.ts
@@ -0,0 +1,66 @@
+import { AuthUser } from '@zenstackhq/runtime';
+import { loadSchema, run, type WeakDbClientContract } from '@zenstackhq/testtools';
+import Decimal from 'decimal.js';
+import superjson from 'superjson';
+
+describe('Filter Function Coverage Tests', () => {
+    it('contains case-sensitive', async () => {
+        const { withPresets } = await loadSchema(
+            `
+            model Foo {
+                id String @id @default(cuid())
+                string String
+                @@allow('all', contains(string, 'a'))
+            }
+            `
+        );
+
+        await expect(withPresets().foo.create({ data: { string: 'bcd' } })).toBeRejectedByPolicy();
+        await expect(withPresets().foo.create({ data: { string: 'bac' } })).toResolveTruthy();
+    });
+
+    it('startsWith', async () => {
+        const { withPresets } = await loadSchema(
+            `
+            model Foo {
+                id String @id @default(cuid())
+                string String
+                @@allow('all', startsWith(string, 'a'))
+            }
+            `
+        );
+
+        await expect(withPresets().foo.create({ data: { string: 'bac' } })).toBeRejectedByPolicy();
+        await expect(withPresets().foo.create({ data: { string: 'abc' } })).toResolveTruthy();
+    });
+
+    it('endsWith', async () => {
+        const { withPresets } = await loadSchema(
+            `
+            model Foo {
+                id String @id @default(cuid())
+                string String
+                @@allow('all', endsWith(string, 'a'))
+            }
+            `
+        );
+
+        await expect(withPresets().foo.create({ data: { string: 'bac' } })).toBeRejectedByPolicy();
+        await expect(withPresets().foo.create({ data: { string: 'bca' } })).toResolveTruthy();
+    });
+
+    it('in', async () => {
+        const { withPresets } = await loadSchema(
+            `
+            model Foo {
+                id String @id @default(cuid())
+                string String
+                @@allow('all', string in ['a', 'b'])
+            }
+            `
+        );
+
+        await expect(withPresets().foo.create({ data: { string: 'c' } })).toBeRejectedByPolicy();
+        await expect(withPresets().foo.create({ data: { string: 'b' } })).toResolveTruthy();
+    });
+});
