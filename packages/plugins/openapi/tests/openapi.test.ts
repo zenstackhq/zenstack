dcc14db1c72c9d71b1e75caaceffb88873946f78
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-03-31 18:12:30 +0800

    fix relation check

--- packages/plugins/openapi/tests/openapi.test.ts
+++ packages/plugins/openapi/tests/openapi.test.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /// <reference types="@types/jest" />
 
 import OpenAPIParser from '@readme/openapi-parser';
 import { loadZModelAndDmmf } from '@zenstackhq/testtools';
 import * as tmp from 'tmp';
+import * as fs from 'fs';
 import generate from '../src';
+import YAML from 'yaml';
+import { isPlugin, Model, Plugin } from '@zenstackhq/sdk/ast';
+import { getLiteral } from '@zenstackhq/sdk';
 
 describe('Open API Plugin Tests', () => {
     it('run plugin', async () => {
@@ -68,9 +73,14 @@ model Bar {
         `);
 
         const { name: output } = tmp.fileSync({ postfix: '.yaml' });
-        generate(model, { schemaPath: modelFile, output }, dmmf);
+
+        const options = buildOptions(model, modelFile, output);
+        generate(model, options, dmmf);
 
         console.log('OpenAPI specification generated:', output);
+
+        const parsed = YAML.parse(fs.readFileSync(output, 'utf-8'));
+        expect(parsed.openapi).toBe('3.1.0');
 
         const api = await OpenAPIParser.validate(output);
         expect(api.paths?.['/user/findMany']?.['get']?.description).toBe('Find users matching the given conditions');
@@ -83,4 +93,73 @@ model Bar {
         expect(api.paths?.['/foo/findMany']).toBeUndefined();
         expect(api.paths?.['/bar/findMany']).toBeUndefined();
     });
+
+    it('options', async () => {
+        const { model, dmmf, modelFile } = await loadZModelAndDmmf(`
+plugin openapi {
+    provider = '${process.cwd()}/dist'
+    specVersion = '3.0.0'
+    title = 'My Awesome API'
+    version = '1.0.0'
+    description = 'awesome api'
+    prefix = '/myapi'
+}
+
+model User {
+    id String @id
+}
+        `);
+
+        const { name: output } = tmp.fileSync({ postfix: '.yaml' });
+        const options = buildOptions(model, modelFile, output);
+        generate(model, options, dmmf);
+
+        console.log('OpenAPI specification generated:', output);
+
+        const parsed = YAML.parse(fs.readFileSync(output, 'utf-8'));
+        expect(parsed.openapi).toBe('3.0.0');
+
+        const api = await OpenAPIParser.validate(output);
+        expect(api.info).toEqual(
+            expect.objectContaining({
+                title: 'My Awesome API',
+                version: '1.0.0',
+                description: 'awesome api',
+            })
+        );
+
+        expect(api.paths?.['/myapi/user/findMany']).toBeTruthy();
+    });
+
+    it('v3.1.0 fields', async () => {
+        const { model, dmmf, modelFile } = await loadZModelAndDmmf(`
+plugin openapi {
+    provider = '${process.cwd()}/dist'
+    summary = 'awesome api'
+}
+
+model User {
+    id String @id
+}
+        `);
+
+        const { name: output } = tmp.fileSync({ postfix: '.yaml' });
+        const options = buildOptions(model, modelFile, output);
+        generate(model, options, dmmf);
+
+        console.log('OpenAPI specification generated:', output);
+
+        const parsed = YAML.parse(fs.readFileSync(output, 'utf-8'));
+        expect(parsed.openapi).toBe('3.1.0');
+
+        const api = await OpenAPIParser.validate(output);
+        expect((api.info as any).summary).toEqual('awesome api');
+    });
 });
+
+function buildOptions(model: Model, modelFile: string, output: string) {
+    const optionFields = model.declarations.find((d): d is Plugin => isPlugin(d))?.fields || [];
+    const options: any = { schemaPath: modelFile, output };
+    optionFields.forEach((f) => (options[f.name] = getLiteral(f.value)));
+    return options;
+}
