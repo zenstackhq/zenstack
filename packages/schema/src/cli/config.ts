003fc1cf3c580b99481ab13d12678807537cef6e
Thomas Horner <thomas.horner@highpointwx.com>
2023-04-15 01:22:21 -0600

    fix: better cli reporting of missing/invalid config file

--- packages/schema/src/cli/config.ts
+++ packages/schema/src/cli/config.ts
@@ -1,6 +1,6 @@
 import { GUARD_FIELD_NAME, TRANSACTION_FIELD_NAME } from '@zenstackhq/sdk';
 import fs from 'fs';
-import z from 'zod';
+import z, {ZodError} from 'zod';
 import { fromZodError } from 'zod-validation-error';
 import { CliError } from './cli-error';
 
@@ -19,22 +19,22 @@ export let config: ConfigType = schema.parse({});
  * Loads and validates CLI configuration file.
  * @returns
  */
-export function loadConfig(filename: string) {
-    if (!fs.existsSync(filename)) {
-        return;
-    }
-
-    let content: unknown;
+export function loadConfig( filename: string ) {
     try {
-        content = JSON.parse(fs.readFileSync(filename, 'utf-8'));
-    } catch {
-        throw new CliError(`Config is not a valid JSON file: ${filename}`);
+        const fileData = fs.readFileSync(filename, `utf-8`);
+        const content = JSON.parse( fileData );
+        config = schema.parse( content );
     }
-
-    const parsed = schema.safeParse(content);
-    if (!parsed.success) {
-        throw new CliError(`Config file ${filename} is not valid: ${fromZodError(parsed.error)}`);
+    catch (err: any) {
+        if ( err?.code === `ENOENT` ) {
+            throw new CliError(`Config file could not be found: ${filename}`)
+        }
+        if ( err instanceof SyntaxError) {
+            throw new CliError(`Config is not a valid JSON file: ${filename}`);
+        }
+        if ( err instanceof ZodError ) {
+            throw new CliError(`Config file ${filename} is not valid: ${fromZodError(err)}`);
+        }
+        throw new CliError( `Error loading config: ${ filename }` );
     }
-
-    config = parsed.data;
 }
