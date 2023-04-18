dd069c85bb24c4e09d35ed63fce506ed777b65b1
Abdullah Hassan <admin@xemah.com>
2023-04-18 19:19:37 +0500

    fix date string zod validation

--- packages/schema/src/plugins/zod/transformer.ts
+++ packages/schema/src/plugins/zod/transformer.ts
@@ -114,7 +114,7 @@ export default class Transformer {
             } else if (inputType.type === 'Boolean') {
                 result.push(this.wrapWithZodValidators('z.boolean()', field, inputType));
             } else if (inputType.type === 'DateTime') {
-                result.push(this.wrapWithZodValidators('z.date()', field, inputType));
+                result.push(this.wrapWithZodValidators('z.union([z.date(), z.string().datetime()])', field, inputType));
             } else if (inputType.type === 'Json') {
                 this.hasJson = true;
 
