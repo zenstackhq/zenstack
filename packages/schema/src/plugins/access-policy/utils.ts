66832cda689e789082b2d480bc5c1593751eca52
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-05-02 16:59:17 -0700

    fix: short-circuit post-read check when policy rules don't dependon model fields

--- packages/schema/src/plugins/access-policy/utils.ts
+++ packages/schema/src/plugins/access-policy/utils.ts
@@ -1,9 +1,10 @@
-import { Expression, isInvocationExpr } from '@zenstackhq/language/ast';
+import { isInvocationExpr } from '@zenstackhq/language/ast';
+import { AstNode } from 'langium/lib/syntax-tree';
 import { isFromStdlib } from '../../language-server/utils';
 
 /**
  * Returns if the given expression is a "future()" method call.
  */
-export function isFutureExpr(expr: Expression) {
-    return !!(isInvocationExpr(expr) && expr.function.ref?.name === 'future' && isFromStdlib(expr.function.ref));
+export function isFutureExpr(node: AstNode) {
+    return !!(isInvocationExpr(node) && node.function.ref?.name === 'future' && isFromStdlib(node.function.ref));
 }
