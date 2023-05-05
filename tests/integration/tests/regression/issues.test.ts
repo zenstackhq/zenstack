66aaec9b48d9eee1a707b26c87eae844dec5d7b9
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-05-05 16:30:30 -0700

    fix: zod plugin issue with lower-case model names

--- tests/integration/tests/regression/issues.test.ts
+++ tests/integration/tests/regression/issues.test.ts
@@ -54,4 +54,18 @@ describe('GitHub issues regression', () => {
         expect(queried.posts[0].zenstack_guard).toBeUndefined();
         expect(queried.posts[0].zenstack_transaction).toBeUndefined();
     });
+
+    it('issue 389', async () => {
+        const { withPolicy } = await loadSchema(`
+        model model {
+            id String @id @default(uuid())
+            value Int
+            @@allow('read', true)
+            @@allow('create', value > 0)
+        }
+        `);
+        const db = withPolicy();
+        await expect(db.model.create({ data: { value: 0 } })).toBeRejectedByPolicy();
+        await expect(db.model.create({ data: { value: 1 } })).toResolveTruthy();
+    });
 });
