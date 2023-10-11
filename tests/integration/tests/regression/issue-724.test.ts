44df433de90d0721435fa8bed6305d8c5222d98f
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-10-11 13:10:32 -0700

    fix: add missing MSSQL related declarations and parameters to stdlib

--- tests/integration/tests/regression/issue-724.test.ts
+++ tests/integration/tests/regression/issue-724.test.ts
@@ -19,14 +19,14 @@ describe('Regression: issue 724', () => {
         }
           
         model LastLocation {
-            LastLocationID       String    @id(map: "PK_LastLocation") @db.UniqueIdentifier
-            UserID                   String    @db.UniqueIdentifier
-            JobID                      String?   @db.UniqueIdentifier
-            Timestamp          DateTime? @db.DateTime
-            Latitude                 String?   @db.VarChar(Max)
-            Longitude                String?   @db.NVarChar(Max)
+            LastLocationID String @id(map: "PK_LastLocation") @db.UniqueIdentifier
+            UserID String @db.UniqueIdentifier
+            JobID String? @db.UniqueIdentifier
+            Timestamp DateTime? @db.DateTime
+            Latitude String? @db.VarChar(Max)
+            Longitude String? @db.NVarChar(Max)
             MostRecentTimestamp DateTime? @db.DateTime
-            CreatedDate               DateTime                    @default(now(), map: "DF_Address_CreatedDate") @db.DateTime
+            CreatedDate DateTime @default(now(), map: "DF_Address_CreatedDate") @db.DateTime
           
             @@index([UserID], map: "IX_UserID")
         }
