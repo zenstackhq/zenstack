c390de10cfa91ae3f954404bc07e0905973b0898
Jason MacDonald <5718527+jasonmacdonald@users.noreply.github.com>
2023-11-01 00:44:52 -0400

    feat: add @@auth option for declaring auth model (#787)
    Co-authored-by: Jason MacDonald <jason.macdonald@veeva.com>
    Co-authored-by: ymc9 <104139426+ymc9@users.noreply.github.com>

--- packages/schema/src/language-server/zmodel-linker.ts
+++ packages/schema/src/language-server/zmodel-linker.ts
@@ -36,7 +36,7 @@ import {
     isReferenceExpr,
     isStringLiteral,
 } from '@zenstackhq/language/ast';
-import { getContainingModel, isFromStdlib } from '@zenstackhq/sdk';
+import { getContainingModel, hasAttribute, isFromStdlib } from '@zenstackhq/sdk';
 import {
     AstNode,
     AstNodeDescription,
@@ -278,9 +278,15 @@ export class ZModelLinker extends DefaultLinker {
                 const model = getContainingModel(node);
 
                 if (model) {
-                    const userModel = getAllDeclarationsFromImports(this.langiumDocuments(), model).find(
-                        (d) => isDataModel(d) && d.name === 'User'
-                    );
+                    let userModel;
+                    userModel = getAllDeclarationsFromImports(this.langiumDocuments(), model).find((d) => {
+                        return isDataModel(d) && hasAttribute(d, '@@auth');
+                    });
+                    if (!userModel) {
+                        userModel = getAllDeclarationsFromImports(this.langiumDocuments(), model).find((d) => {
+                            return isDataModel(d) && d.name === 'User';
+                        });
+                    }
                     if (userModel) {
                         node.$resolvedType = { decl: userModel, nullable: true };
                     }
