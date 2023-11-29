bf85ceb3ef84ca68a6c370c6d6349af1edb79428
Jiasheng <jiashengguo@outlook.com>
2023-11-29 20:19:25 +0000

    feat:Support ZModel format command in CLI (#869)

---
+++ tests/integration/tests/cli/format.test.ts
@@ -0,0 +1,43 @@
+import * as fs from 'fs';
+import * as tmp from 'tmp';
+import { createProgram } from '../../../../packages/schema/src/cli';
+describe('CLI format test', () => {
+    let origDir: string;
+
+    beforeEach(() => {
+        origDir = process.cwd();
+        const r = tmp.dirSync({ unsafeCleanup: true });
+        console.log(`Project dir: ${r.name}`);
+        process.chdir(r.name);
+    });
+
+    afterEach(() => {
+        process.chdir(origDir);
+    });
+
+    it('basic format', async () => {
+        const model = `
+        datasource db {provider="sqlite" url="file:./dev.db"}
+        generator client {provider = "prisma-client-js"}
+        model Post {id Int @id() @default(autoincrement())users User[]}`;
+
+        const formattedModel = `
+datasource db {
+    provider="sqlite"
+    url="file:./dev.db"
+}
+generator client {
+    provider = "prisma-client-js"
+}
+model Post {
+    id Int @id() @default(autoincrement())
+    users User[]
+}`;
+        // set up schema
+        fs.writeFileSync('schema.zmodel', model, 'utf-8');
+        const program = createProgram();
+        await program.parseAsync(['format'], { from: 'user' });
+
+        expect(fs.readFileSync('schema.zmodel', 'utf-8')).toEqual(formattedModel);
+    });
+});
