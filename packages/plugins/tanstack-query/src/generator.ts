cc98e306559d7729d96d4ed77cda2815454fbb8f
Yiming <yiming@whimslab.io>
2024-01-29 16:07:55 +0800

    fix: tanstack-query, fix the incorrect query typing when user provides a custom selector (#967)

--- packages/plugins/tanstack-query/src/generator.ts
+++ packages/plugins/tanstack-query/src/generator.ts
@@ -84,9 +84,9 @@ function generateQueryHook(
     const capOperation = upperCaseFirst(operation);
 
     const argsType = overrideInputType ?? `Prisma.${model}${capOperation}Args`;
-    const inputType = `Prisma.SelectSubset<T, ${argsType}>`;
+    const inputType = `Prisma.SelectSubset<TArgs, ${argsType}>`;
 
-    let defaultReturnType = `Prisma.${model}GetPayload<T>`;
+    let defaultReturnType = `Prisma.${model}GetPayload<TArgs>`;
     if (optimisticUpdate) {
         defaultReturnType += '& { $optimistic?: boolean }';
     }
@@ -95,11 +95,16 @@ function generateQueryHook(
     }
 
     const returnType = overrideReturnType ?? defaultReturnType;
-    const optionsType = makeQueryOptions(target, returnType, infinite, version);
+    const optionsType = makeQueryOptions(target, 'TQueryFnData', 'TData', infinite, version);
 
     const func = sf.addFunction({
         name: `use${infinite ? 'Infinite' : ''}${capOperation}${model}`,
-        typeParameters: overrideTypeParameters ?? [`T extends ${argsType}`],
+        typeParameters: overrideTypeParameters ?? [
+            `TArgs extends ${argsType}`,
+            `TQueryFnData = ${returnType} `,
+            'TData = TQueryFnData',
+            'TError = DefaultError',
+        ],
         parameters: [
             {
                 name: optionalInput ? 'args?' : 'args',
@@ -129,7 +134,9 @@ function generateQueryHook(
 
     func.addStatements([
         makeGetContext(target),
-        `return ${infinite ? 'useInfiniteModelQuery' : 'useModelQuery'}('${model}', \`\${endpoint}/${lowerCaseFirst(
+        `return ${
+            infinite ? 'useInfiniteModelQuery' : 'useModelQuery'
+        }<TQueryFnData, TData, TError>('${model}', \`\${endpoint}/${lowerCaseFirst(
             model
         )}/${operation}\`, args, options, fetch${optimisticUpdate ? ', optimisticUpdate' : ''});`,
     ]);
@@ -403,7 +410,7 @@ function generateModelHooks(
             'aggregate',
             false,
             false,
-            `Prisma.Get${modelNameCap}AggregateType<T>`
+            `Prisma.Get${modelNameCap}AggregateType<TArgs>`
         );
     }
 
@@ -415,16 +422,27 @@ function generateModelHooks(
             useName = model.name;
         }
 
+        const returnType = `{} extends InputErrors ? 
+        Array<PickEnumerable<Prisma.${modelNameCap}GroupByOutputType, TArgs['by']> &
+          {
+            [P in ((keyof TArgs) & (keyof Prisma.${modelNameCap}GroupByOutputType))]: P extends '_count'
+              ? TArgs[P] extends boolean
+                ? number
+                : Prisma.GetScalarType<TArgs[P], Prisma.${modelNameCap}GroupByOutputType[P]>
+              : Prisma.GetScalarType<TArgs[P], Prisma.${modelNameCap}GroupByOutputType[P]>
+          }
+        > : InputErrors`;
+
         const typeParameters = [
-            `T extends Prisma.${useName}GroupByArgs`,
-            `HasSelectOrTake extends Prisma.Or<Prisma.Extends<'skip', Prisma.Keys<T>>, Prisma.Extends<'take', Prisma.Keys<T>>>`,
+            `TArgs extends Prisma.${useName}GroupByArgs`,
+            `HasSelectOrTake extends Prisma.Or<Prisma.Extends<'skip', Prisma.Keys<TArgs>>, Prisma.Extends<'take', Prisma.Keys<TArgs>>>`,
             `OrderByArg extends Prisma.True extends HasSelectOrTake ? { orderBy: Prisma.${useName}GroupByArgs['orderBy'] }: { orderBy?: Prisma.${useName}GroupByArgs['orderBy'] },`,
-            `OrderFields extends Prisma.ExcludeUnderscoreKeys<Prisma.Keys<Prisma.MaybeTupleToUnion<T['orderBy']>>>`,
-            `ByFields extends Prisma.MaybeTupleToUnion<T['by']>`,
+            `OrderFields extends Prisma.ExcludeUnderscoreKeys<Prisma.Keys<Prisma.MaybeTupleToUnion<TArgs['orderBy']>>>`,
+            `ByFields extends Prisma.MaybeTupleToUnion<TArgs['by']>`,
             `ByValid extends Prisma.Has<ByFields, OrderFields>`,
-            `HavingFields extends Prisma.GetHavingFields<T['having']>`,
+            `HavingFields extends Prisma.GetHavingFields<TArgs['having']>`,
             `HavingValid extends Prisma.Has<ByFields, HavingFields>`,
-            `ByEmpty extends T['by'] extends never[] ? Prisma.True : Prisma.False`,
+            `ByEmpty extends TArgs['by'] extends never[] ? Prisma.True : Prisma.False`,
             `InputErrors extends ByEmpty extends Prisma.True
             ? \`Error: "by" must not be empty.\`
             : HavingValid extends Prisma.False
@@ -440,8 +458,8 @@ function generateModelHooks(
                     \` in "having" needs to be provided in "by"\`,
                     ]
             }[HavingFields]
-            : 'take' extends Prisma.Keys<T>
-            ? 'orderBy' extends Prisma.Keys<T>
+            : 'take' extends Prisma.Keys<TArgs>
+            ? 'orderBy' extends Prisma.Keys<TArgs>
             ? ByValid extends Prisma.True
                 ? {}
                 : {
@@ -450,8 +468,8 @@ function generateModelHooks(
                     : \`Error: Field "\${P}" in "orderBy" needs to be provided in "by"\`
                 }[OrderFields]
             : 'Error: If you provide "take", you also need to provide "orderBy"'
-            : 'skip' extends Prisma.Keys<T>
-            ? 'orderBy' extends Prisma.Keys<T>
+            : 'skip' extends Prisma.Keys<TArgs>
+            ? 'orderBy' extends Prisma.Keys<TArgs>
             ? ByValid extends Prisma.True
                 ? {}
                 : {
@@ -467,18 +485,10 @@ function generateModelHooks(
                 ? never
                 : \`Error: Field "\${P}" in "orderBy" needs to be provided in "by"\`
             }[OrderFields]`,
+            `TQueryFnData = ${returnType}`,
+            `TData = TQueryFnData`,
+            `TError = DefaultError`,
         ];
-
-        const returnType = `{} extends InputErrors ? 
-        Array<PickEnumerable<Prisma.${modelNameCap}GroupByOutputType, T['by']> &
-          {
-            [P in ((keyof T) & (keyof Prisma.${modelNameCap}GroupByOutputType))]: P extends '_count'
-              ? T[P] extends boolean
-                ? number
-                : Prisma.GetScalarType<T[P], Prisma.${modelNameCap}GroupByOutputType[P]>
-              : Prisma.GetScalarType<T[P], Prisma.${modelNameCap}GroupByOutputType[P]>
-          }
-        > : InputErrors`;
 
         generateQueryHook(
             target,
@@ -489,7 +499,7 @@ function generateModelHooks(
             false,
             false,
             returnType,
-            `Prisma.SubsetIntersection<T, Prisma.${useName}GroupByArgs, OrderByArg> & InputErrors`,
+            `Prisma.SubsetIntersection<TArgs, Prisma.${useName}GroupByArgs, OrderByArg> & InputErrors`,
             typeParameters
         );
     }
@@ -504,7 +514,7 @@ function generateModelHooks(
             'count',
             false,
             true,
-            `T extends { select: any; } ? T['select'] extends true ? number : Prisma.GetScalarType<T['select'], Prisma.${modelNameCap}CountAggregateOutputType> : number`
+            `TArgs extends { select: any; } ? TArgs['select'] extends true ? number : Prisma.GetScalarType<TArgs['select'], Prisma.${modelNameCap}CountAggregateOutputType> : number`
         );
     }
 }
@@ -552,12 +562,13 @@ function makeBaseImports(target: TargetFramework, version: TanStackVersion) {
         `import { useModelQuery, useInfiniteModelQuery, useModelMutation } from '${runtimeImportBase}/${target}';`,
         `import type { PickEnumerable, CheckSelect } from '${runtimeImportBase}';`,
         `import metadata from './__model_meta';`,
+        `type DefaultError = Error;`,
     ];
     switch (target) {
         case 'react':
             return [
                 `import type { UseMutationOptions, UseQueryOptions, UseInfiniteQueryOptions, InfiniteData } from '@tanstack/react-query';`,
-                `import { RequestHandlerContext, getHooksContext } from '${runtimeImportBase}/${target}';`,
+                `import { getHooksContext } from '${runtimeImportBase}/${target}';`,
                 ...shared,
             ];
         case 'vue':
@@ -573,7 +584,7 @@ function makeBaseImports(target: TargetFramework, version: TanStackVersion) {
                 ...(version === 'v5'
                     ? [`import type { InfiniteData, StoreOrVal } from '@tanstack/svelte-query';`]
                     : []),
-                `import { SvelteQueryContextKey, type RequestHandlerContext, getHooksContext } from '${runtimeImportBase}/${target}';`,
+                `import { getHooksContext } from '${runtimeImportBase}/${target}';`,
                 ...shared,
             ];
         default:
@@ -581,24 +592,30 @@ function makeBaseImports(target: TargetFramework, version: TanStackVersion) {
     }
 }
 
-function makeQueryOptions(target: string, returnType: string, infinite: boolean, version: TanStackVersion) {
+function makeQueryOptions(
+    target: string,
+    returnType: string,
+    dataType: string,
+    infinite: boolean,
+    version: TanStackVersion
+) {
     switch (target) {
         case 'react':
             return infinite
                 ? version === 'v4'
-                    ? `Omit<UseInfiniteQueryOptions<${returnType}>, 'queryKey'>`
-                    : `Omit<UseInfiniteQueryOptions<${returnType}, unknown, InfiniteData<${returnType}>>, 'queryKey'>`
-                : `Omit<UseQueryOptions<${returnType}>, 'queryKey'>`;
+                    ? `Omit<UseInfiniteQueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>`
+                    : `Omit<UseInfiniteQueryOptions<${returnType}, TError, InfiniteData<${dataType}>>, 'queryKey'>`
+                : `Omit<UseQueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>`;
         case 'vue':
-            return `Omit<Use${infinite ? 'Infinite' : ''}QueryOptions<${returnType}>, 'queryKey'>`;
+            return `Omit<Use${infinite ? 'Infinite' : ''}QueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>`;
         case 'svelte':
             return infinite
                 ? version === 'v4'
-                    ? `Omit<CreateInfiniteQueryOptions<${returnType}>, 'queryKey'>`
-                    : `StoreOrVal<Omit<CreateInfiniteQueryOptions<${returnType}, unknown, InfiniteData<${returnType}>>, 'queryKey'>>`
+                    ? `Omit<CreateInfiniteQueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>`
+                    : `StoreOrVal<Omit<CreateInfiniteQueryOptions<${returnType}, TError, InfiniteData<${dataType}>>, 'queryKey'>>`
                 : version === 'v4'
-                ? `Omit<CreateQueryOptions<${returnType}>, 'queryKey'>`
-                : `StoreOrVal<Omit<CreateQueryOptions<${returnType}>, 'queryKey'>>`;
+                ? `Omit<CreateQueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>`
+                : `StoreOrVal<Omit<CreateQueryOptions<${returnType}, TError, ${dataType}>, 'queryKey'>>`;
         default:
             throw new PluginError(name, `Unsupported target: ${target}`);
     }
