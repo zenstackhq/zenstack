aba77842276fb08c250f4a1ac9edbc5e7751d14a
Jonathan Stevens <jonathan.stevens@resnovas.com>
2024-02-20 22:13:40 +0000

    feat(pkg-utils.ts): add findNodeModulesFile function to pkg-utils.ts
    Signed-off-by: Jonathan Stevens <jonathan.stevens@resnovas.com>

--- packages/schema/src/utils/pkg-utils.ts
+++ packages/schema/src/utils/pkg-utils.ts
@@ -37,6 +37,32 @@ export function findUp<e extends boolean = false>(names: string[], cwd: string =
     return findUp(names, up, multiple, result);
 }
 
+/**
+ * A function that finds a file within the node_modules directory by the provided name. 
+ * It first searches for the node_modules directory using the findUp function, and if not found, returns undefined. 
+ * It then splits the provided name into folder and file parts. 
+ * It finds the location of the folder in the node_modules directory by checking for its existence. 
+ * If the folder is not found, it returns undefined. 
+ * It then constructs the file location by joining the folder location and the file name with a '.zmodel' extension if the file does not already have an extension. 
+ * Returns the file location if it exists, otherwise returns undefined.
+ * @author Jonathan Stevens (TGTGamer)
+ *
+ * @export
+ * @param name A string representing the name of the file to find in the node_modules folder
+ * @returns Path to a specific file within the node_modules directory
+ */
+export function findNodeModulesFile(name: string, cwd: string = process.cwd()) {
+    if (!name) return undefined;
+    const nodeModules = findUp(['node_modules'], cwd, true);
+    if (!nodeModules) return undefined;
+    const split = name.split('/')
+    const folder = split.slice(0, -1).join('/');
+    const folderLocation = nodeModules.find((dir) =>fs.existsSync(path.join(dir, folder)));
+    if (!folderLocation) return undefined;
+    const fileLocation = path.join(folderLocation, name);
+    return fs.existsSync(fileLocation) ? fileLocation : undefined;
+}
+
 function getPackageManager(projectPath = '.'): PackageManagers {
     const lockFile = findUp(['yarn.lock', 'pnpm-lock.yaml', 'package-lock.json'], projectPath);
 
