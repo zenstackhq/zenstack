7eb9670fe5f80f61a0752150419510b893665475
augustin <azzerty23@hotmail.fr>
2024-02-20 13:28:05 +0100

    add test

--- tests/integration/tests/enhancements/with-policy/auth.test.ts
+++ tests/integration/tests/enhancements/with-policy/auth.test.ts
@@ -505,4 +505,33 @@ describe('With Policy: auth() test', () => {
             ])
         );
     });
+
+    it('Default auth() without user context', async () => {
+        const { enhance } = await loadSchema(
+            `
+        model User {
+            id String @id
+            posts Post[]
+
+            @@allow('all', true)
+        }
+
+        model Post {
+            id String @id @default(uuid())
+            title String
+            author User @relation(fields: [authorId], references: [id])
+            authorId String @default(auth().id)
+
+            @@allow('all', auth().id != null)
+        }
+        `
+        );
+
+        const db = enhance();
+        await expect(db.user.create({ data: { id: 'userId-1' } })).toResolveTruthy();
+        await expect(db.post.create({ data: { title: 'title' } })).rejects.toThrow(
+            'Using `auth()` in `@default` requires a user context'
+        );
+        await expect(db.post.findMany({})).toResolveTruthy();
+    });
 });
