6259fd9b998e4dc58301902da4cfbf956593bd8c
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-02-21 17:24:01 -0800

    chore: set up ZENSTACK_TEST environment variable during test setup

--- script/test-global-setup.ts
+++ script/test-global-setup.ts
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 
 export default function globalSetup() {
-    if (!fs.existsSync(path.join(__dirname, '.test/scaffold/package-lock.json'))) {
+    if (!fs.existsSync(path.join(__dirname, '../.test/scaffold/package-lock.json'))) {
         console.error(`Test scaffold not found. Please run \`pnpm test-scaffold\` first.`);
         process.exit(1);
     }
