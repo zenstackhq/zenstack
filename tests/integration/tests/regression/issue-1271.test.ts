6edfc89c29572b3608c2609ee654bc69468ce66c
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-21 22:22:06 +0800

    more fixes

--- tests/integration/tests/regression/issue-1271.test.ts
+++ tests/integration/tests/regression/issue-1271.test.ts
@@ -47,7 +47,7 @@ describe('issue 1271', () => {
 
         const test = await db.test.create({
             data: {
-                key: 'hello',
+                key: 'test1',
             },
         });
         const anotherTest = await db.anotherTest.create({
@@ -85,18 +85,16 @@ describe('issue 1271', () => {
         expect(updated.linkingTable).toHaveLength(1);
         expect(updated.linkingTable[0]).toMatchObject({ another_test_id: anotherTest.id });
 
-        const newKey = 'newKey';
-        const newLocale = 'ZH';
-        const created = await db.test.upsert({
+        const test2 = await db.test.upsert({
             where: {
                 key_locale: {
-                    key: newKey,
-                    locale: newLocale,
+                    key: 'test2',
+                    locale: 'locale2',
                 },
             },
             create: {
-                key: newKey,
-                locale: newLocale,
+                key: 'test2',
+                locale: 'locale2',
                 linkingTable: {
                     create: {
                         another_test_id: anotherTest.id,
@@ -114,8 +112,81 @@ describe('issue 1271', () => {
                 linkingTable: true,
             },
         });
-        expect(created).toMatchObject({ key: newKey, locale: newLocale });
-        expect(created.linkingTable).toHaveLength(1);
-        expect(created.linkingTable[0]).toMatchObject({ another_test_id: anotherTest.id });
+        expect(test2).toMatchObject({ key: 'test2', locale: 'locale2' });
+        expect(test2.linkingTable).toHaveLength(1);
+        expect(test2.linkingTable[0]).toMatchObject({ another_test_id: anotherTest.id });
+
+        const linkingTable = test2.linkingTable[0];
+
+        // connectOrCreate: connect case
+        const test3 = await db.test.create({
+            data: {
+                key: 'test3',
+                locale: 'locale3',
+            },
+        });
+        console.log('test3 created:', test3);
+        const updated2 = await db.linkingTable.update({
+            where: {
+                test_id_another_test_id: {
+                    test_id: linkingTable.test_id,
+                    another_test_id: linkingTable.another_test_id,
+                },
+            },
+            data: {
+                test: {
+                    connectOrCreate: {
+                        where: {
+                            key_locale: {
+                                key: test3.key,
+                                locale: test3.locale,
+                            },
+                        },
+                        create: {
+                            key: 'test4',
+                            locale: 'locale4',
+                        },
+                    },
+                },
+                another_test: { connect: { id: anotherTest.id } },
+            },
+            include: { test: true },
+        });
+        expect(updated2).toMatchObject({
+            test: expect.objectContaining({ key: 'test3', locale: 'locale3' }),
+            another_test_id: anotherTest.id,
+        });
+
+        // connectOrCreate: create case
+        const updated3 = await db.linkingTable.update({
+            where: {
+                test_id_another_test_id: {
+                    test_id: updated2.test_id,
+                    another_test_id: updated2.another_test_id,
+                },
+            },
+            data: {
+                test: {
+                    connectOrCreate: {
+                        where: {
+                            key_locale: {
+                                key: 'test4',
+                                locale: 'locale4',
+                            },
+                        },
+                        create: {
+                            key: 'test4',
+                            locale: 'locale4',
+                        },
+                    },
+                },
+                another_test: { connect: { id: anotherTest.id } },
+            },
+            include: { test: true },
+        });
+        expect(updated3).toMatchObject({
+            test: expect.objectContaining({ key: 'test4', locale: 'locale4' }),
+            another_test_id: anotherTest.id,
+        });
     });
 });
