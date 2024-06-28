9ec728725f10d9b04fa923b401a5207430b862f7
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-06-28 02:19:23 +0200

    add test

--- tests/integration/tests/enhancements/with-policy/prisma-omit.test.ts
+++ tests/integration/tests/enhancements/with-policy/prisma-omit.test.ts
@@ -1,7 +1,7 @@
 import { loadSchema } from '@zenstackhq/testtools';
 
 describe('prisma omit', () => {
-    it('test', async () => {
+    it('per query', async () => {
         const { prisma, enhance } = await loadSchema(
             `
             model User {
@@ -48,6 +48,72 @@ describe('prisma omit', () => {
 
         found = await db.user.findFirst({
             select: { value: true, profile: { omit: { level: true } } },
+        });
+        console.log(found);
+        expect(found.age).toBeUndefined();
+        expect(found.value).toEqual(10);
+        expect(found.profile.level).toBeUndefined();
+    });
+
+    it('global', async () => {
+        const { prisma, enhance } = await loadSchema(
+            `
+            model User {
+              id   String @id @default(cuid())
+              name String
+              profile Profile?
+              age Int
+              value Int
+              @@allow('all', true)
+            }
+            
+            model Profile {
+              id   String @id @default(cuid())
+              user User   @relation(fields: [userId], references: [id])
+              userId String @unique
+              level Int
+              @@allow('all', true)
+            }
+            `,
+            {
+                previewFeatures: ['omitApi'],
+                prismaClientOptions: {
+                    omit: {
+                        user: { age: true, value: false },
+                        profile: { level: true },
+                    },
+                },
+            }
+        );
+
+        await prisma.user.create({
+            data: {
+                name: 'John',
+                age: 25,
+                value: 10,
+                profile: {
+                    create: { level: 2 },
+                },
+            },
+        });
+
+        const db = enhance();
+
+        let found = await db.user.findFirst({
+            include: { profile: true },
+        });
+        expect(found.age).toBeUndefined();
+        expect(found.value).toEqual(10);
+        expect(found.profile.level).toBeUndefined();
+
+        found = await db.user.findFirst({
+            omit: { age: false },
+            include: { profile: true },
+        });
+        expect(found.age).toBe(25);
+
+        found = await db.user.findFirst({
+            select: { value: true, profile: true },
         });
         console.log(found);
         expect(found.age).toBeUndefined();
