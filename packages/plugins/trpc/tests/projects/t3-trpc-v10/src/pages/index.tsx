8a9bcd07412d3e6b5dccc4dea1e7e11ab733990f
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-09-05 21:42:03 -0700

    fix: fix tests

--- packages/plugins/trpc/tests/projects/t3-trpc-v10/src/pages/index.tsx
+++ packages/plugins/trpc/tests/projects/t3-trpc-v10/src/pages/index.tsx
@@ -4,7 +4,10 @@ import styles from './index.module.css';
 export default function Home() {
     const hello = api.greet.hello.useQuery({ text: 'from tRPC' });
     const posts = api.post.findMany.useQuery({ where: { published: true }, include: { author: true } });
-    const postsTransformed = api.post.findMany.useQuery({}, { select: (data) => data.map((p) => ({ title: p.name })) });
+    const postsTransformed = api.post.findMany.useQuery(
+        {},
+        { select: (data) => data.map((p) => ({ id: p.id, title: p.name })) }
+    );
 
     return (
         <>
@@ -12,11 +15,11 @@ export default function Home() {
                 {hello.data && <h1 className={styles.title}>{hello.data.greeting}</h1>}
                 {posts.data &&
                     posts.data.map((post) => (
-                        <p>
+                        <p key={post.id}>
                             {post.name} by {post.author.email}
                         </p>
                     ))}
-                {postsTransformed.data && postsTransformed.data.map((post) => <p>{post.title}</p>)}
+                {postsTransformed.data && postsTransformed.data.map((post) => <p key={post.id}>{post.title}</p>)}
             </main>
         </>
     );
