c0db38902b8837ba7877069b53b9903f1e389af2
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-09-22 20:08:24 -0700

    chore: update trpc test code

--- packages/plugins/trpc/tests/projects/t3-trpc-v10/src/utils/api.ts
+++ packages/plugins/trpc/tests/projects/t3-trpc-v10/src/utils/api.ts
@@ -18,7 +18,7 @@ const getBaseUrl = () => {
 };
 
 /** A set of type-safe react-query hooks for your tRPC API. */
-export const api = createTRPCNext<AppRouter>({
+export const api = createTRPCNext<AppRouter, 'post'>({
     config() {
         return {
             /**
