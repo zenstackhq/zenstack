465a6138170f6f17ed3551f3cd20a571c67ff017
Thomas Sunde Nielsen <me@thomassnielsen.com>
2024-10-01 10:27:41 +0200

    Create with compound id test

--- packages/server/tests/api/rest.test.ts
+++ packages/server/tests/api/rest.test.ts
@@ -1603,6 +1603,33 @@ describe('REST server tests', () => {
 
                     expect(r.status).toBe(404);
                 });
+
+                describe('compound id', () => {
+                    beforeEach(async () => {
+                        await prisma.user.create({ data: { myId: 'user1', email: 'user1@abc.com' } });
+                        await prisma.post.create({
+                            data: { id: 1, title: 'Post1' },
+                        });
+                    });
+
+                    it('create single', async () => {
+                        const r = await handler({
+                            method: 'post',
+                            path: '/postLike',
+                            query: {},
+                            requestBody: {
+                                data: {
+                                    type: 'postLike',
+                                    id: `1${idDivider}user1`,
+                                    attributes: { userId: 'user1', postId: 1, superLike: false },
+                                },
+                            },
+                            prisma,
+                        });
+
+                        expect(r.status).toBe(201);
+                    });
+                });
             });
 
             describe('PUT', () => {
