e7f6b54a1370474215df817f4f2f672ad5be0dd0
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-11-12 17:34:54 -0800

    update

--- tests/regression/tests/issue-prisma-extension.test.ts
+++ tests/regression/tests/issue-prisma-extension.test.ts
@@ -39,7 +39,7 @@ describe('issue prisma extension', () => {
     });
 
     it('enhance extended client', async () => {
-        const { enhanceRaw, prisma } = await loadSchema(
+        const { enhanceRaw, prisma, prismaModule } = await loadSchema(
             `
             model Post {
                 id Int @id
@@ -55,18 +55,20 @@ describe('issue prisma extension', () => {
         await prisma.post.create({ data: { id: 1, title: 'post1', published: true } });
         await prisma.post.create({ data: { id: 2, title: 'post2', published: false } });
 
-        const extended = prisma.$extends({
-            model: {
-                post: {
-                    findManyListView: async (args: any) => {
-                        return { view: true, data: await prisma.post.findMany(args) };
+        const ext = prismaModule.defineExtension((_prisma: any) => {
+            return _prisma.$extends({
+                model: {
+                    post: {
+                        findManyListView: async (args: any) => {
+                            return { view: true, data: await prisma.post.findMany(args) };
+                        },
                     },
                 },
-            },
+            });
         });
 
-        await expect(extended.post.findMany()).resolves.toHaveLength(2);
-        await expect(extended.post.findManyListView()).resolves.toMatchObject({
+        await expect(prisma.$extends(ext).post.findMany()).resolves.toHaveLength(2);
+        await expect(prisma.$extends(ext).post.findManyListView()).resolves.toMatchObject({
             view: true,
             data: [
                 { id: 1, title: 'post1', published: true },
@@ -74,9 +76,10 @@ describe('issue prisma extension', () => {
             ],
         });
 
-        const db = enhanceRaw(extended);
-        await expect(db.post.findMany()).resolves.toHaveLength(1);
-        await expect(db.post.findManyListView()).resolves.toMatchObject({
+        const enhanced = enhanceRaw(prisma.$extends(ext));
+        await expect(enhanced.post.findMany()).resolves.toHaveLength(1);
+        // findManyListView internally uses the un-enhanced client
+        await expect(enhanced.post.findManyListView()).resolves.toMatchObject({
             view: true,
             data: [
                 { id: 1, title: 'post1', published: true },
