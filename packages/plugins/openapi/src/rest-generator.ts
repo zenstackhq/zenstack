4d9530650bd264bf36f8c14485d6b05f3e2d6288
Thomas Sunde Nielsen <me@thomassnielsen.com>
2024-11-15 10:40:55 +0100

    Expose compound id as separate fields always

--- packages/plugins/openapi/src/rest-generator.ts
+++ packages/plugins/openapi/src/rest-generator.ts
@@ -857,10 +857,8 @@ export class RESTfulOpenAPIGenerator extends OpenAPIGeneratorBase {
 
     private generateModelEntity(model: DataModel, mode: 'read' | 'create' | 'update'): OAPI.SchemaObject {
         const idFields = model.fields.filter((f) => isIdField(f));
-        // For compound ids each component is also exposed as a separate fields for read operations,
-        // but not required for write operations
-        const fields =
-            idFields.length > 1 && mode !== 'update' ? model.fields : model.fields.filter((f) => !isIdField(f));
+        // For compound ids each component is also exposed as a separate fields.
+        const fields = idFields.length > 1 ? model.fields : model.fields.filter((f) => !isIdField(f));
 
         const attributes: Record<string, OAPI.SchemaObject> = {};
         const relationships: Record<string, OAPI.ReferenceObject | OAPI.SchemaObject> = {};
