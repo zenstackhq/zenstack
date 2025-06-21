68e5a627e77d86db9a10a54cb2ea14aca563761e
FTB_lag <tabolskyy.git@gmail.com>
2025-06-21 15:05:34 +0300

    chore(runtime): reindent traverse file tabs to spaces

--- packages/runtime/src/local-helpers/traverse-raw.ts
+++ packages/runtime/src/local-helpers/traverse-raw.ts
@@ -1,66 +1,66 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 type Cb = (
-	data: {
-		path: readonly string[];
-		key: string;
-		value: any;
-		update: (nextValue: any) => void;
-	}
+    data: {
+        path: readonly string[];
+        key: string;
+        value: any;
+        update: (nextValue: any) => void;
+    }
 ) => void;
 
 function walk<T>(root: T, cb: Cb) {
-	const path: string[] = [];
-	const parents: any[] = [];
+    const path: string[] = [];
+    const parents: any[] = [];
 
-	function walker(node: any) {
-		const isObject = typeof node === 'object' && node !== null;
-		const isCircular = isObject && parents.some((p) => p === node);
-		const key = path[path.length - 1];
+    function walker(node: any) {
+        const isObject = typeof node === 'object' && node !== null;
+        const isCircular = isObject && parents.some((p) => p === node);
+        const key = path[path.length - 1];
 
-		let keepGoing = true;
+        let keepGoing = true;
 
-		function update(nextValue: any) {
-			if (path.length) {
-				const parent = parents[parents.length - 1];
-				parent[key] = nextValue;
-				node = nextValue;
-			}
+        function update(nextValue: any) {
+            if (path.length) {
+                const parent = parents[parents.length - 1];
+                parent[key] = nextValue;
+                node = nextValue;
+            }
 
-			keepGoing = false;
-		}
+            keepGoing = false;
+        }
 
-		cb({
-			path: [...path],
-			key,
-			value: node,
-			update,
-		});
+        cb({
+            path: [...path],
+            key,
+            value: node,
+            update,
+        });
 
-		if (!keepGoing) return node;
+        if (!keepGoing) return node;
 
-		if (isObject && !isCircular) {
-			parents.push(node);
+        if (isObject && !isCircular) {
+            parents.push(node);
 
-			Object.keys(node).forEach((key) => {
-				path.push(key);
+            Object.keys(node).forEach((key) => {
+                path.push(key);
 
-				walker(node[key]);
+                walker(node[key]);
 
-				path.pop();
-			});
+                path.pop();
+            });
 
-			parents.pop();
-		}
+            parents.pop();
+        }
 
-		return node;
-	}
+        return node;
+    }
 
-	return walker(root);
+    return walker(root);
 }
 
 function traverse<T>(obj: T, cb: Cb) {
-	return walk(obj, cb);
+    return walk(obj, cb);
 }
 
 module.exports = traverse;
