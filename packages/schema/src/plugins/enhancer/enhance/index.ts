e3421bcd1ecdff7c704ade0978551d70cf396e46
Eugen Istoc <eugenistoc@gmail.com>
2025-07-04 23:17:31 -0400

    refactor: change JSON fields type definitions file extension from .d.ts to .ts

--- packages/schema/src/plugins/enhancer/enhance/index.ts
+++ packages/schema/src/plugins/enhancer/enhance/index.ts
@@ -493,7 +493,7 @@ export type Enhanced<Client> =
         const project = new Project();
 
         // Create a shared file for all JSON fields type definitions
-        const jsonFieldsFile = project.createSourceFile(path.join(this.outDir, 'json-fields.d.ts'), undefined, {
+        const jsonFieldsFile = project.createSourceFile(path.join(this.outDir, 'json-fields.ts'), undefined, {
             overwrite: true,
         });
 
