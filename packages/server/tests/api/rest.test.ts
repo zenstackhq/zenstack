b9899318f8d67516d300eb43f5ad8a858891acae
ymc9 <104139426+ymc9@users.noreply.github.com>
2025-07-16 11:07:41 +0800

    add a sample test case

--- packages/server/tests/api/rest.test.ts
+++ packages/server/tests/api/rest.test.ts
@@ -3013,4 +3013,65 @@ describe('REST server tests', () => {
             expect(r.body.data.attributes.enabled).toBe(false);
         });
     });
+
+    describe('REST server tests - model name mapping', () => {
+        const schema = `
+    model User {
+        id String @id @default(cuid())
+        name String
+        posts Post[]
+    }
+    
+    model Post {
+        id String @id @default(cuid())
+        title String
+        author User? @relation(fields: [authorId], references: [id])
+        authorId String?
+    }
+    `;
+        beforeAll(async () => {
+            const params = await loadSchema(schema);
+            prisma = params.prisma;
+            zodSchemas = params.zodSchemas;
+            modelMeta = params.modelMeta;
+
+            const _handler = makeHandler({
+                endpoint: 'http://localhost/api',
+                modelNameMapping: {
+                    myUser: 'user',
+                    myPost: 'post',
+                },
+            });
+            handler = (args) =>
+                _handler({ ...args, zodSchemas, modelMeta, url: new URL(`http://localhost/${args.path}`) });
+        });
+
+        it('works with name mapping', async () => {
+            // using original model name
+            await expect(
+                handler({
+                    method: 'post',
+                    path: '/user',
+                    query: {},
+                    requestBody: { data: { type: 'user', attributes: { name: 'User1' } } },
+                    prisma,
+                })
+            ).resolves.toMatchObject({
+                status: 400,
+            });
+
+            // using mapped model name
+            await expect(
+                handler({
+                    method: 'post',
+                    path: '/myUser',
+                    query: {},
+                    requestBody: { data: { type: 'user', attributes: { name: 'User1' } } },
+                    prisma,
+                })
+            ).resolves.toMatchObject({
+                status: 201,
+            });
+        });
+    });
 });
