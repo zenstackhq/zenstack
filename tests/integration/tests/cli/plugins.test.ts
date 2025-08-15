a975457f698e342d49359fe0a089d7cbcccb6a5d
Yiming Cao <yiming@whimslab.io>
2025-08-15 09:27:13 +0800

    chore: update to prisma 6.14 (#2223)

--- tests/integration/tests/cli/plugins.test.ts
+++ tests/integration/tests/cli/plugins.test.ts
@@ -75,7 +75,7 @@ describe('CLI Plugins Tests', () => {
             'swr',
             '@tanstack/react-query@5.56.x',
             '@trpc/server',
-            '@prisma/client@6.13.x',
+            '@prisma/client@6.14.x',
             `${path.join(__dirname, '../../../../.build/zenstackhq-language-' + ver + '.tgz')}`,
             `${path.join(__dirname, '../../../../.build/zenstackhq-sdk-' + ver + '.tgz')}`,
             `${path.join(__dirname, '../../../../.build/zenstackhq-runtime-' + ver + '.tgz')}`,
@@ -85,7 +85,7 @@ describe('CLI Plugins Tests', () => {
         const devDepPkgs = [
             'typescript',
             '@types/react',
-            'prisma@6.13.x',
+            'prisma@6.14.x',
             `${path.join(__dirname, '../../../../.build/zenstack-' + ver + '.tgz')}`,
             `${path.join(__dirname, '../../../../.build/zenstackhq-tanstack-query-' + ver + '.tgz')}`,
             `${path.join(__dirname, '../../../../.build/zenstackhq-swr-' + ver + '.tgz')}`,
