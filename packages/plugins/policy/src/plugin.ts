ece062f02b5fc088db65ec974a6cb8d1e0a8b850
Pavel Kudinov <pkudinov@gmail.com>
2026-03-21 03:58:50 +0800

    fix(policy): allow dangerous raw SQL opt-in (#2502)

--- packages/plugins/policy/src/plugin.ts
+++ packages/plugins/policy/src/plugin.ts
@@ -1,9 +1,14 @@
 import { type OnKyselyQueryArgs, type RuntimePlugin } from '@zenstackhq/orm';
 import type { SchemaDef } from '@zenstackhq/orm/schema';
+import type { PolicyPluginOptions } from './options';
 import { check } from './functions';
 import { PolicyHandler } from './policy-handler';
 
+export type { PolicyPluginOptions } from './options';
+
 export class PolicyPlugin implements RuntimePlugin<SchemaDef, {}, {}, {}> {
+    constructor(private readonly options: PolicyPluginOptions = {}) {}
+
     get id() {
         return 'policy' as const;
     }
@@ -23,7 +28,7 @@ export class PolicyPlugin implements RuntimePlugin<SchemaDef, {}, {}, {}> {
     }
 
     onKyselyQuery({ query, client, proceed }: OnKyselyQueryArgs<SchemaDef>) {
-        const handler = new PolicyHandler<SchemaDef>(client);
+        const handler = new PolicyHandler<SchemaDef>(client, this.options);
         return handler.handle(query, proceed);
     }
 }
