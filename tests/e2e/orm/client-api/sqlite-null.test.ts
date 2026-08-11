5438853ffde6ce622d13181856e0783309bad5c5
sanny-io <sannysherief@gmail.com>
2026-08-11 22:54:55 +0000

    chore: add test

---
+++ tests/e2e/orm/client-api/sqlite-null.test.ts
@@ -0,0 +1,27 @@
+import { createTestClient, getTestDbProvider } from '@zenstackhq/testtools';
+import { describe, expect, it } from 'vitest';
+
+const provider = getTestDbProvider();
+
+describe.skipIf(provider !== 'sqlite')('sqlite null tests', () => {
+    it('allows writing null to a optional Decimal fields', async () => {
+        const db = await createTestClient(
+            `
+model User {
+    id      Int @id @default(autoincrement())
+    balance Decimal?
+}
+`,
+        );
+
+        await expect(
+            db.user.create({
+                data: {
+                    balance: null,
+                },
+            }),
+        ).resolves.toMatchObject({
+            balance: null,
+        });
+    });
+});
