bafb47918da8f6e738d44b170c8ffb833e0fe21c
sanny-io <sannysherief@gmail.com>
2026-09-04 10:36:07 +0000

    (server): pass `argsPayload` as-is

--- packages/server/src/api/rest/index.ts
+++ packages/server/src/api/rest/index.ts
@@ -718,10 +718,7 @@ export class RestApiHandler<Schema extends SchemaDef = SchemaDef> implements Api
         const argsPayload = method === 'POST' ? requestBody : query;
 
         // support SuperJSON request payload format
-        const { result: processedArgsPayload, error } = await processSuperJsonRequestPayload({
-            data: argsPayload,
-            meta: (requestBody as any)?.meta,
-        });
+        const { result: processedArgsPayload, error } = await processSuperJsonRequestPayload(argsPayload as any);
         if (error) {
             return this.makeProcBadInputErrorResponse(error);
         }
