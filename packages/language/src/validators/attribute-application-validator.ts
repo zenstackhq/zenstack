094a2b7310a75ffed8297e25ba71d7bf0c355a89
sanny-io <sannysherief@gmail.com>
2026-09-24 01:26:27 +0000

    fix(language): remove parent primitive type def check

--- packages/language/src/validators/attribute-application-validator.ts
+++ packages/language/src/validators/attribute-application-validator.ts
@@ -564,8 +564,7 @@ function assignableToAttributeParam(
             // If the field is JSON, and the attribute is @default, the argument must be a JSON string
             // (design inherited from Prisma)
             const dstIsJson = attr.$container.type.type === 'Json' || hasAttribute(attr.$container, '@json');
-            const isInPrimitiveTypeDef = isPrimitiveTypeDef(attr.$container.$container);
-            if (dstIsJson && isInPrimitiveTypeDef && attr.decl.ref?.name === '@default') {
+            if (dstIsJson && attr.decl.ref?.name === '@default') {
                 if (attr.$container.type.array && attr.$container.type.type === 'Json') {
                     // Json[] default value, must be array of JSON strings
                     if (isArrayExpr(arg.value) && arg.value.items.every((item) => isLiteralJsonString(item))) {
